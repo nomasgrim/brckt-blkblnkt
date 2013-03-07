6dae12c9cde32e275d92fde131ea3bab41dc9604
cbare1208 <cbarendsen@vml.com>
2013-03-07 12:38:21 -0600

    added some d3 action

--- brckt-blkblnkt.js
+++ brckt-blkblnkt.js
@@ -1,5 +1,14 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
+    d3.select("body").selectAll("p")
+        .data([4, 8, 15, 16, 23, 42])
+      .enter().append("p")
+        .text(function(d) { return "I’m number " + d + "!"; });
+    d3.selectAll("p")
+        .data([4, 8, 15, 16, 23, 42])
+        .style("font-size", function(d) { return d + "px"; })
+        .style("color", function() {return "hsl(" + Math.random() * 360 + ",100%,50%)";});
+    
     return "Welcome to brckt-blkblnkt.";
   };
 
@@ -10,6 +19,7 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
 }
 
 if (Meteor.isServer) {
